a9b5e91c4cf2106fae58746fad94769b81af5636
Freddy Maldonado <contacto@freddymaldonado.com>
2024-03-19 16:30:39 -0300

    correccciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
-var tooltipTriggerList = Array.prototype.slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipTriggerList = Array.prototype.slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+      if (targetElement) {
+          targetElement.scrollIntoView({
+              behavior: 'smooth'
+          });
+      }
+  });
+});
+
+
 
